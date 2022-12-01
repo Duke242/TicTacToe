d5bc0f2b15fd2833430dc55ed1facca9d5b1bca7
Duke <aboukhodr21@gmail.com>
2022-11-30 23:01:56 -0500

    changed the css a bit

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const Gameboard = (() => {return {board: Array(9)};})();
-
-const box = document.querySelectorAll('.box')
-const boxes = Array.from(box)
-boxes.forEach(element => addEventListener('click', (e) => { 
-    Gameboard.board[e.target.id] = "X"
-    console.log(e.target.id)
-    console.log(Gameboard.board)
-    for (i = 0; i < Gameboard.board.length; i++) { 
-        box[i].innerHTML = Gameboard.board[i]
-    }
-}))
-
-
+const Gameboard = (() => {
+    return { board: Array(9) };
+  })();
+  
+  const box = document.querySelectorAll(".box");
+  const boxes = Array.from(box);
+  boxes.forEach((element) =>
+    element.addEventListener("click", (e) => {
+      Gameboard.board[e.target.id] = "X";
+      console.log(e.target.id);
+      console.log(Gameboard.board);
+      for (let i = 0; i < Gameboard.board.length; i++) {
+        box[i].innerHTML = Gameboard.board[i];
+      }
+    })
+  );
+  
